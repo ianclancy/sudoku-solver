4d13347cf14ef1c22bf0d9941454fdfa1ae1b51e
ianclancy <ianrclancy@gmail.com>
2017-12-10 20:03:04 -0500

    Solve puzzle button

--- react/src/components/Square.js
+++ react/src/components/Square.js
@@ -3,14 +3,17 @@ import React from 'react';
 const Square = props => {
   let squareIndex = props.rowIndex * 9 + props.indexInRow
   let squareValue = ""
-  if (props.puzzle[squareIndex]) {
-    squareValue = props.puzzle[squareIndex]
+  if (!!props.puzzle[squareIndex] || props.puzzle[squareIndex] == 0) {
+    return (
+      <div className="square">
+        {props.puzzle[squareIndex] + 1}
+      </div>
+    )
+  } else {
+    return (
+      <input type="text" className="number-input" maxLength="1"/>
+    )
   }
-  return (
-    <div className="square">
-      {squareValue}
-    </div>
-  )
 }
 
 export default Square;
