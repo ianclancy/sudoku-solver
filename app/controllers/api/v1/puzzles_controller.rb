7863271e18325789897cb30ffa904cb76b0eeb59
ianclancy <ianrclancy@gmail.com>
2017-12-17 13:00:53 -0500

    Puzzles migration

---
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::PuzzlesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :puzzles
+    end
+  end
+
   root 'static_pages#index'
 end
